bbf617a6da5861108ddfba97eeff49021fc6fe05
saso1212 <saso.davidovik@gmail.com>
2018-02-26 02:41:38 +0100

    Shopping Cart update

---
+++ shoppingCart/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+   
+    $('#save').on('click',function(){
+        debugger;
+        $('#shopingCard').empty();
+        
+
+    })
+    // $(document).on('click', '.btn-block', function (e){
+    //     e.preventDefault();
+    //     debugger;
+    //     $('#shopingCard').empty();
+    // });
+})
